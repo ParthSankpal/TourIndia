d9595705d3bfb5e6e5149b4382edeee45f9d7d0b
ParthSankpal <parth.sankpal47@gmail.com>
2023-05-29 00:27:35 +0530

    static

--- my-project/src/components/Main.jsx
+++ my-project/src/components/Main.jsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import Rajasthan from '../assets/Rajasthan.png';
+import { BsChevronRight } from "react-icons/bs";
 
 const Main = () => {
+
+    const cardData=[
+        {id:1, image:[Rajasthan],location:"rajasthan", discription:"Rajasthan VIP Tour: Desert Safari & Day Trip", cost:"INR 15000",mrp:"INR 20000" ,discount:"25%"},
+        {id:1, image:[Rajasthan],location:"rajasthan", discription:"Rajasthan VIP Tour: Desert Safari & Day Trip", cost:"INR 15000",mrp:"INR 20000" ,discount:"25%"},
+        {id:1, image:[Rajasthan],location:"rajasthan", discription:"Rajasthan VIP Tour: Desert Safari & Day Trip", cost:"INR 15000",mrp:"INR 20000" ,discount:"25%"},
+        {id:1, image:[Rajasthan],location:"rajasthan", discription:"Rajasthan VIP Tour: Desert Safari & Day Trip", cost:"INR 15000",mrp:"INR 20000" ,discount:"25%"}
+
+    ]
   return (
-    <div>Main</div>
+    <>
+    <section className='pb-5'>
+    <header>
+        <div className='mx-28 my-16 text-[#374772] justify-between flex'>
+            <h2 className='font-El_Messiri text-4xl'>
+                What to Experience
+            </h2>
+            <Link to="/visaapplication" class='text-[#374772] font-Merienda font-medium bg-transparent border border-[#374772]  px-4 py-2 rounded-full'>
+                View more
+            </Link>
+        </div>
+    </header>
+    <div className='mx-24 '>
+        <div className='grid grid-cols-1  gap-14 lg:grid-cols-4'>
+            
+                {cardData.map((cardData)=>(
+                    <div key={cardData.id} className='rounded-2xl flex flex-col h-96 shadow-lg hover:shadow-2xl '>
+                        <div>
+                            <img src={cardData.image} alt='Rajasthan' className='rounded-lg'/>
+                        </div>
+                        <div >
+                            <div className='mt-5 mx-8 text-justify  '>
+                                <div className='font-Merienda text-[#68696c84] text-xs pb-2 text-opacity-10 '>
+                                    {cardData.location}
+                                </div>
+                                <div className='text-[#374772] text-md font-bold font-Merienda'>
+                                    {cardData.discription}
+                                </div>
+                                <div className=' text-[#374772bd] text-sm pt-4 font-Merienda '>
+                                    Starting from
+                                </div>
+                            </div>
+                            <div className='my-2 ml-8  flex text-justify'>
+                                <div className='w-2/5 text-[#374772] text-sm '>
+                                    {cardData.cost}
+                                </div>
+                                <span className='line-through text-[#374772] text-sm '>
+                                    {cardData.mrp}
+                                </span>
+                                <span className='text-white text-center text-xs w-8 h-4 text-[#ffffff] rounded-md bg-regal-blue ml-2'>
+                                    {cardData.discount}
+                                </span>
+                            </div>
+                            <div className='text-[#374772] flex mt-4 border-t pt-3 mx-8 text-justify justify-between'>
+                                <span >Book Now</span>
+                                <span ><BsChevronRight/></span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            
+            
+            
+        </div>
+    </div>
+        <section>
+
+        </section>
+
+    </section>
+    </>
   )
 }
 
 export default Main
+
+
+//  <li className=''>
+//                 <div className='rounded-2xl h-96 shadow-lg hover:shadow-2xl '>
+//                     <div>
+//                         <img src={Rajasthan} alt='Rajasthan' className='rounded-lg'/>
+//                     </div>
+//                     <div >
+//                         <div className='mt-5 mx-8 text-justify  '>
+//                             <div className='font-Merienda text-[#68696c84] text-xs pb-2 text-opacity-10 '>
+//                                 Rajasthan
+//                             </div>
+//                             <div className='text-[#374772] text-md font-bold font-Merienda'>
+//                                 Rajasthan VIP Tour: Desert Safari & Day Trip
+//                             </div>
+//                             <div className=' text-[#374772bd] text-sm pt-4 font-Merienda '>
+//                                 Starting from
+//                             </div>
+//                         </div>
+//                         <div className='my-2 ml-8  flex text-justify'>
+//                             <div className='w-2/5 text-[#374772] text-sm '>
+//                                 INR 15000
+//                             </div>
+//                             <span className='line-through text-[#374772] text-sm '>
+//                                 INR 20000
+//                             </span>
+//                             <span className='text-white text-center text-xs w-8 h-4 text-[#ffffff] rounded-md bg-regal-blue ml-2'>
+//                                 25% sale
+//                             </span>
+//                         </div>
+//                         <div className='text-[#374772] flex mt-4 border-t pt-3 mx-8 text-justify justify-between'>
+//                             <span >Book Now</span>
+//                             <span ><BsChevronRight/></span>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </li> 
--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import logo from '../../src/assets/logo.jpeg';
+import search from '../assets/search.png';
+import profile from '../assets/profile.png';
+import language from '../assets/language.svg';
 
 const Navbar = () => {
+
+    const [dropdownShow, setDropdownShow] = useState(false)
+    const handleToggle=() => {
+        setDropdownShow(!dropdownShow)
+    }
   return (
-    <div>Navbar</div>
+    <>
+        <nav className='text-[#374772] w-full justify-between h-10   flex flex-nowrap bg-[#fffff3] lg:px-11 sm: px-2 py-2 border-b border-b-[#e6ebf4]'>
+            <Link className='font-Merienda hover:font-semibold ml-14 text-sm'>
+                Contact Us
+            </Link>
+            <button type='button' onClick={handleToggle} className='inline-flex  mr-10 w-24 justify-center gap-x-1.5 rounded-md bg-[#fffff3] px-3 py-1 text-sm  text-gray-900 hover:bg-[#fffff0]' id="menu-button" aria-expanded="true" aria-haspopup="true">
+                <img src={language} alt='glob' className='w-4'/>
+                <a href="#" class="text-gray-700 font-Merienda hover:font-semibold  block pb-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Eng</a>
+                {dropdownShow && 
+                <div class="absolute right-18 z-10 border-[#374772] mt-2 w-24 origin-top-right rounded-md bg-[#fffff3] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                    <div class="py-1" role="none">
+                        <a href="#" class="text-gray-700 font-Merienda hover:font-semibold border-[#374772] border-b-2 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Eng</a>
+                        <a href="#" class="text-gray-700 font-Merienda hover:font-semibold border-[#374772] border-b-2 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Hin</a>
+                        <a href="#" class="text-gray-700 font-Merienda hover:font-semibold block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Mar</a>
+                        
+                    </div>
+                </div>}
+                
+            </button>
+        </nav>
+        <header className='text-[#374772]  w-full flex justify-between items-center  bg-[#fffff3] lg:px-14 sm: px-8 py-4 border-b border-b-[#e6ebf4]'>
+            <div className='flex font-Merienda flex-nowrap align-middle gap-8'>
+                <div>
+                    <Link >
+                        <img src={logo} alt='logo' className='w-24  object-contain'/>
+                    </Link>
+                </div>
+                <Link className='font-Merienda hover:font-semibold'>
+                    See & Do
+                </Link>
+                <Link className='font-Merienda hover:font-semibold'>
+                    Plan Your Trip
+                </Link>
+                <Link className='font-Merienda hover:font-semibold'>
+                    Travel Essentials
+                </Link>
+                <Link className='font-Merienda hover:font-semibold'>
+                    India Calender
+                </Link>
+                <Link className='font-Merienda hover:font-semibold '>
+                    Experiences
+                </Link>
+                <Link className='font-Merienda hover:font-semibold'>
+                    India Map
+                </Link>
+            </div>
+            <div className='flex flex-nowrap align-middle gap-4'>
+                <Link className='font-inner w-6 pt-2 font-medium'>
+                    <img src={search} alt='search'/>
+                </Link>
+                <Link className='font-inner w-6 pt-2 font-medium'>
+                    <img src={profile} alt='profile'/>
+                </Link>
+                <Link to="/visaapplication" className='text-[#fffff3] font-inner font-Merienda font-medium bg-[#374772] text-white px-4 py-2 rounded-full'>
+                    Apply for eVisa
+                </Link>
+                
+            </div>
+          </header>
+    </>
   )
 }
 
