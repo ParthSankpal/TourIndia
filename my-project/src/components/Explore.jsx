bc47a6d5c356e76996dc151bb389a66edb18bafe
ParthSankpal <parth.sankpal47@gmail.com>
2023-06-04 23:07:58 +0530

    .

--- my-project/src/components/Explore.jsx
+++ my-project/src/components/Explore.jsx
@@ -1,54 +1,92 @@
-import React from 'react';
-import explorebg from '../assets/explorebg.png';
-import Rajasthan from '../assets/Rajasthan.png';
-import Kerla from '../assets/Kerla.png';
-import TajMahal2 from '../assets/TajMahal2.png';
-import riversunset from '../assets/riversunset.png';
-import Mountains from '../assets/Mountains.png';
-import pattern from '../assets/pattern.png';
+import React from "react";
+import explorebg from "../assets/explorebg.png";
+import Rajasthan from "../assets/Rajasthan.png";
+import Kerla from "../assets/Kerla.png";
+import TajMahal2 from "../assets/TajMahal2.png";
+import riversunset from "../assets/riversunset.png";
+import Mountains from "../assets/Mountains.png";
+import pattern from "../assets/pattern.png";
 
-const cardData=[
-  {id:1, image:[Rajasthan],location:"rajasthan", discription:"Rajasthan VIP Tour: Desert Safari & Day Trip", cost:"INR 15000",mrp:"INR 20000" ,discount:"25%"},
-  {id:1, image:[Kerla],location:"Kerla", discription:" Kerala: God's Own Country", cost:"INR 15000",mrp:"INR 20000" ,discount:"25%"},
-  {id:1, image:[TajMahal2],location:"Delhi", discription:"Taj Mahal: A love story set in stone.", cost:"INR 15000" },
-  {id:1, image:[riversunset],location:"Andhra", discription:"Rajasthan VIP Tour: Desert Safari & Day Trip", cost:"INR 15000",mrp:"INR 20000" ,discount:"25%"},
-  {id:1, image:[Mountains],location:"Maharahtra", discription:"Rajasthan VIP Tour: Desert Safari & Day Trip", cost:"INR 15000",mrp:"INR 20000" ,discount:"25%"},
-  {id:1, image:[Mountains],location:"Maharahtra", discription:"Rajasthan VIP Tour: Desert Safari & Day Trip", cost:"INR 15000",mrp:"INR 20000" ,discount:"25%"}
-
-]
-
-
+const cardData = [
+  {
+    id: 1,
+    image: [Rajasthan],
+    location: "rajasthan",
+    discription: "Rajasthan VIP Tour: Desert Safari & Day Trip",
+    cost: "INR 15000",
+    mrp: "INR 20000",
+    discount: "25%",
+  },
+  {
+    id: 1,
+    image: [Kerla],
+    location: "Kerla",
+    discription: " Kerala: God's Own Country",
+    cost: "INR 15000",
+    mrp: "INR 20000",
+    discount: "25%",
+  },
+  {
+    id: 1,
+    image: [TajMahal2],
+    location: "Delhi",
+    discription: "Taj Mahal: A love story set in stone.",
+    cost: "INR 15000",
+  },
+  {
+    id: 1,
+    image: [riversunset],
+    location: "Andhra",
+    discription: "Rajasthan VIP Tour: Desert Safari & Day Trip",
+    cost: "INR 15000",
+    mrp: "INR 20000",
+    discount: "25%",
+  },
+  {
+    id: 1,
+    image: [Mountains],
+    location: "Maharahtra",
+    discription: "Rajasthan VIP Tour: Desert Safari & Day Trip",
+    cost: "INR 15000",
+    mrp: "INR 20000",
+    discount: "25%",
+  },
+  {
+    id: 1,
+    image: [Mountains],
+    location: "Maharahtra",
+    discription: "Rajasthan VIP Tour: Desert Safari & Day Trip",
+    cost: "INR 15000",
+    mrp: "INR 20000",
+    discount: "25%",
+  },
+];
 
 const Explore = () => {
   return (
-    <section className='mt-32' >
-        <header className=' bg-[#faf7f3] text-3xl py-8 text-justify px-32 font-Merienda text-[#374772]'>
-          Explore Top Destinations
-        </header>
-        <main className="" style={{ backgroundImage: `url(${explorebg})`}}>
-              <div className='pt-8'>
-                Map
-              </div>
-              <div className='flex'>
-                <div class="snap-x flex ">
-                  <div class="snap-center scroll-ml-6   rounded-lg">
-                    <img src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
-                  </div>
-                  <div class="snap-center scroll-ml-6 rounded-lg">
-                    <img src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
-                  </div>
-                  <div class="snap-center scroll-ml-6 rounded-lg">
-                    <img src="https://images.unsplash.com/photo-1622890806166-111d7f6c7c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
-                  </div>
-                  <div class="snap-center scroll-ml-6 rounded-lg">
-                    <img src="https://images.unsplash.com/photo-1590523277543-a94d2e4eb00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
-                  </div>
-                  
-                </div>
-              </div>
-        </main>
+    <section
+      className="mt-32 bg-cover"
+      style={{ backgroundImage: `url(${explorebg})` }}
+    >
+      <header className=" text-3xl py-8 text-justify px-32 font-Merienda text-[#374772]">
+        Explore Top Destinations
+      </header>
+      <main className="mx-44">
+        <div className="pt-8">Map</div>
+        <div className="">
+          <div className="flex space-x-4">
+            <div
+              className=" w-80 pr-7 h-full inline-block p-2 cursor-pointer rounded-2xl bg-center bg-cover duration-500 "
+            >Hi</div>
+            <div
+              
+              className=" w-80 pr-7 h-full inline-block p-2 cursor-pointer rounded-2xl bg-center bg-cover duration-500 "
+            >Bye</div>
+          </div>
+        </div>
+      </main>
     </section>
-  )
-}
+  );
+};
 
-export default Explore
+export default Explore;
--- my-project/src/components/Features.jsx
+++ my-project/src/components/Features.jsx
@@ -4,30 +4,39 @@ import Shopping from "../assets/shopping.jpg";
 import { AiOutlineHeart } from "react-icons/ai";
 import { BsChevronRight, BsChevronLeft } from "react-icons/bs";
 
-const cardData = [{ url: [Bartender] }, { url: [Shopping] }];
+const cardData = [
+  {
+    heading: "Eat and Drink",
+    disc: "From Kashmir to Kanyakumari authentic coffee, you will taste something that is not found anywhere in the world.",
+    url: [Bartender],
+  },
+  {
+    heading: "Shopping in India",
+    disc: "There are a lot to take with you back home from Saudi shops and creativity.",
+    url: [Shopping],
+  },
+];
 
 import { Link } from "react-router-dom";
 
 const Features = () => {
-
   const [currentIndex, setCurrentIndex] = useState(0);
   const [secCUrrentIndex, setSecCurrentIndex] = useState(1);
+  const [transitionClass, setTransitionClass] = useState('');
+
   const previousImage = () => {
-    setCurrentIndex((prevIndex) =>
-      (prevIndex - 1 + cardData.length) % cardData.length
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + cardData.length) % cardData.length
     );
-    setSecCurrentIndex((prevIndex) =>
-      (prevIndex - 1 + cardData.length) % cardData.length
+    setSecCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + cardData.length) % cardData.length
     );
-    
+    setTransitionClass('translate-x-full');
   };
   const nextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % cardData.length);
     setSecCurrentIndex((prevIndex) => (prevIndex + 1) % cardData.length);
-
   };
-
-  
 
   return (
     <>
@@ -36,26 +45,30 @@ const Features = () => {
         style={{ backgroundImage: `url(${cardData[secCUrrentIndex].url})` }}
       >
         <div className="overflow-hidden">
-          <header className="text-4xl mx-36 pb-8 border-b-2">
+          <header className="text-4xl drop-shadow-2xl mx-36 pb-8 border-b-2">
             Authentically India
           </header>
           <div className=" ">
-            <div className=" mt-42 ml-36">
+            <div className=" relative  mt-42 ml-36">
               <div className=" cursor-pointer">
-                <div className="flex items-center text-sm font-bold">
-                  <Link to="">
+                <div className="flex items-center pt-6 text-sm font-bold">
+                  <Link
+                    to=""
+                    className="rounded-full w-8 h-8 bg-[#eeeeee79] flex  items-center justify-center"
+                  >
                     <AiOutlineHeart />
                   </Link>
                   <div className="px-4">Save to my favorites</div>
                 </div>
               </div>
-              <div className="text-6xl my-9">Eat and Drink</div>
+              <div className="text-6xl mt-20 mb-5">
+                {currentIndex ? cardData[0].heading : cardData[1].heading}
+              </div>
               <div className="w-1/3">
-                From Kashmir to Kanyakumari authentic coffee, you will taste
-                something that is not found anywhere in the world.
+                {currentIndex ? cardData[0].disc : cardData[1].disc}
               </div>
 
-              <div className="  ">
+              <div className="py-12">
                 <div className="  w-36">
                   <Link
                     to="/visaapplication"
@@ -66,34 +79,37 @@ const Features = () => {
                   </Link>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="w-70 h-48 relative left-[1000px]   flex  gap-4">
-            <div className="text-5xl items-center grid py-6 justify-between rounded-full">
-              <div className="  bg-[#374772] text-white  rounded-full">
-                <BsChevronRight onClick={previousImage} />
-              </div>
-              <div className=" bg-[#374772] text-white  rounded-full">
-                <BsChevronLeft onClick={nextImage}  />
-              </div>
-            </div>
+              <div className=" absolute w-70 h-48 left-[65%] bottom-5  flex  gap-4">
+                <div className=" items-center grid py-6 justify-between rounded-full">
+                  <div className="  bg-[#374772] h-8 w-8 text-white  rounded-full">
+                    <BsChevronRight onClick={previousImage} />
+                  </div>
+                  <div className=" bg-[#374772] h-8 w-8 text-white rounded-full">
+                    <BsChevronLeft onClick={nextImage} />
+                  </div>
+                </div>
 
-            <div className="flex space-x-4">
-              <div
-                style={{
-                  backgroundImage: `url(${cardData[currentIndex].url})`,
-                }}
-                className=" w-80 pr-7 h-full inline-block p-2 cursor-pointer rounded-2xl bg-center bg-cover duration-500 "
-              />
-              <div
-                style={{ backgroundImage: `url(${cardData[secCUrrentIndex].url})` }}
-                className=" w-80 pr-7 h-full inline-block p-2 cursor-pointer rounded-2xl bg-center bg-cover duration-500 "
-              />
+                <div className="flex space-x-4">
+                  <div
+                    style={{
+                      backgroundImage: `url(${cardData[currentIndex].url})`,
+                    }}
+                    className=" w-80 pr-7 h-full inline-block p-2 cursor-pointer rounded-2xl ${transitionClass} bg-center bg-cover transition-transform duration-500 transform translate-x-0 "
+                    onClick={previousImage}
+                  />
+                  <div
+                    style={{
+                      backgroundImage: `url(${cardData[secCUrrentIndex].url})`,
+                    }}
+                    className=" w-80 pr-7 h-full inline-block p-2 cursor-pointer rounded-2xl ${transitionClass} bg-center bg-cover transition-transform duration-500 transform translate-x-0 "
+                    onClick={nextImage}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className=" flex ml-56 over  "></div>
       </section>
     </>
   );
